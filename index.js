7cd51d83e80c07ff1374098a0d84868f09fd89b7
Merak Bourque <xtremeyou@hotmail.com>
2024-07-23 17:51:49 -0400

    added lat/long locaiton finding

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss')
+const needle = require('needle')
 
 
 fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -14,9 +14,33 @@ const fetchMyIP = function (callback) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
-    const ip = body.ip
+
+    const ip = body.ip;
     callback(null, ip)
   });
 }
 
-module.exports = { fetchMyIP }
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const latitude = body.latitude
+    const longitude = body.longitude
+    callback(null, {latitude, longitude});
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+};
